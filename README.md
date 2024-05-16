89e435e5cc69f04f5a44fa288213b28b0230b5d4
alexpetmecky <petmeckyalex@gmail.com>
2024-05-15 18:31:00 -0700

    Commit message goes here

---
+++ README.md
@@ -0,0 +1,9 @@
+# Test Jdeploy
+
+{{ appDescription }}
+
+[Releases](https://github.com/AlexPetmecky/TEST_JDEPLOY/releases)
+
+## About this Project
+
+This project was generated using the Swing project template from [jDeploy](https://www.jdeploy.com), a tool to deploy Java desktop apps on Windows, Mac.
---
+++ src/test/java/com/github/alexpetmecky/test_jdeploy/TEST_JDEPLOYTest.java
@@ -0,0 +1,38 @@
+package com.github.alexpetmecky.test_jdeploy;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class TEST_JDEPLOYTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public TEST_JDEPLOYTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( TEST_JDEPLOYTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
